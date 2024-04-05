e362d745666de9cc58183a0620bd8446107f29d9
Ravi <ravi.prasad@accolitedigital.com>
2024-04-05 10:47:02 +0530

    Adress resolved

--- src/app/User.ts
+++ src/app/User.ts
@@ -5,6 +5,15 @@ export class User{
     email: string | undefined
     phone: string | undefined
     website: string | undefined
-    address: any | undefined
-    company: string | undefined
+    address: {
+        street:string
+        suite: string
+        city: string
+        zipcode: string
+    }| undefined
+    company: {
+        name:string
+        catchPhrase: string
+        bs: string
+    }| undefined
 }
